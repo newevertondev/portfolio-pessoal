1c389ade1b23d3df026bef9f9f3ed90e1c7e5d26
Neweverton Fernandes da Silva <160629211+newevertondev@users.noreply.github.com>
2026-03-24 13:59:40 -0300

    separar código JavaScript do HTML
    Movido o JavaScript embutido no HTML para um arquivos externo script.js para melhorar organização e manutenção.

---
+++ pages/aboutMe/script-aboutMe.js
@@ -0,0 +1,17 @@
+    function enviarWhats(event) {
+         event.preventDefault();
+
+        const nome = document.getElementById ('nome').value;
+        const mensagem = document.getElementById('mensagem').value;
+        const telefone = '5534999985714'
+
+        const texto = `Olá! Me chamo ${nome}, ${mensagem}`
+        const msgFormatada = encodeURIComponent (texto)
+
+        const url = `https://wa.me/${telefone}/?text=${msgFormatada}`
+
+        window.open(url, '_blank')
+
+        console.log(url)
+    }
+
